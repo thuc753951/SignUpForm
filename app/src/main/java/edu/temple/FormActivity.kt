14831a4fd412f2911bbcbe2733d39130399a44b5
thuc luong <dungluong67@gmail.com>
2021-09-16 12:30:08 -0400

    starting part 2

--- app/src/main/java/edu/temple/FormActivity.kt
+++ app/src/main/java/edu/temple/FormActivity.kt
@@ -7,5 +7,6 @@ class FormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
